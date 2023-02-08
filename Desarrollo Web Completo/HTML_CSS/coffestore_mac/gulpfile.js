dde7db645cf41e8148ede77110ad4401feecb6c7
macrountree <mcrntree@gmail.com>
2023-02-08 05:02:28 -0700

    node...

--- Desarrollo Web Completo/HTML_CSS/coffestore_mac/gulpfile.js
+++ Desarrollo Web Completo/HTML_CSS/coffestore_mac/gulpfile.js
@@ -1,11 +1,12 @@
 //gfhgf
 
+//     gulp exporta     Require importa los modulos instalados en este caso la dependencia gulp
 const { src, dest } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function css(done) {
   //compilar SASS
-  //Identificar Archivo, Compilar y guardar el .css
+  //Identificar Archivo(src), Compilar(pipe(sass(Este se compila))) y guardar el .css (pipe(dest(lo mandamos a build/css)))
   src("scr/scss/styles.scss").pipe(sass()).pipe(dest("build/css"));
   done();
 }
