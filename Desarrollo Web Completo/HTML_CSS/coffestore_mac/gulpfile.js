488e5379317d0644c5165b5f35bbe0134529cff1
macrountree <mcrntree@gmail.com>
2023-02-09 09:54:44 -0700

    avif_webp

--- Desarrollo Web Completo/HTML_CSS/coffestore_mac/gulpfile.js
+++ Desarrollo Web Completo/HTML_CSS/coffestore_mac/gulpfile.js
@@ -2,12 +2,19 @@
 
 //     gulp exporta     Require importa los modulos instalados en este caso la dependencia gulp
 const { src, dest, watch, series } = require("gulp");
+
+//-----CSS-----SASS------------------------
+
 //const sass requiere gulp sass para que se compile
 const sass = require("gulp-sass")(require("sass"));
-
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
+//--------IMAGENES--------------------
+const imagemin = require("gulp-imagemin"); //instalamos la version 7.1.0 de gulp-imagemin para que soporte la funcion require
+const webp = require("gulp-webp"); //instalamos con npm i --save-dev gulp-webp.. lo utilizamos para convertir
+const avif = require("gulp-avif");
+//----------FUNCIONES------------
 function css(done) {
   //compilar SASS
   //Identificar Archivo(src), Compilar(pipe(sass(Este se compila))) y guardar el .css (pipe(dest(lo mandamos a build/css)))
@@ -16,6 +23,30 @@ function css(done) {
     .pipe(postcss([autoprefixer()]))
     .pipe(dest("build/css"));
   done();
+}
+
+function imgs(done) {
+  src("src/img/**/*") //identifica img en esta ruta
+    .pipe(imagemin({ optimizationLevel: 3 })) //mimificamos imagenes de la ruta disminuyendo su tamañp
+    .pipe(dest("build/img"));
+  done();
+}
+function imgswebp() {
+  const options = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(webp(options))
+    .pipe(dest("build/img")); //todas las imagenes seran filtradas por png y jpg
+}
+
+function imgsavif() {
+  const options = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(avif(options))
+    .pipe(dest("build/img"));
 }
 //JS no permite repetición de metodos asi que declaramos dev() para que no cree conflictos con watch()
 function dev() {
@@ -27,10 +58,16 @@ function dev() {
   /*Aqui nos dice que (**)son todos los archivos dentro de la carpeta src y *.scss son
   todos los archivos con la extension .scss */
   watch("src/**/*.scss", css);
+  watch("src/img/**/*", imgs);
 }
 exports.css = css;
 exports.dev = dev;
+exports.imgs = imgs;
+exports.imgswebp = imgswebp;
+exports.imgsavif = imgsavif;
+
 /*Series ejecuta la primera tarea (css) y cuando termina empieza la otra (dev) */
-exports.default = series(css, dev);
+
+exports.default = series(imgs, imgswebp, imgsavif, css, dev);
 /* Parallel ejecuta todas al mismo tiempo (dejar watch hasta el final (dev)
 exports.default= parallel(css,dev);*/
