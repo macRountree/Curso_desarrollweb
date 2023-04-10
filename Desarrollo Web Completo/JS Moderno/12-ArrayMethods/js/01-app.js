2ee562d315bc276e6d915fdbf92b30f908f6d03e
Macrountree <a219206225@unison.mx>
2023-04-10 16:44:52 -0700

    JS_arraynethods

--- Desarrollo Web Completo/JS Moderno/12-ArrayMethods/js/01-app.js
+++ Desarrollo Web Completo/JS Moderno/12-ArrayMethods/js/01-app.js
@@ -1,10 +1,29 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
 
 const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+  { nombre: "Monitor 27 Pulgadas", precio: 500 },
+  { nombre: "Televisión", precio: 100 },
+  { nombre: "Tablet", precio: 200 },
+  { nombre: "Audifonos", precio: 300 },
+  { nombre: "Teclado", precio: 400 },
+  { nombre: "Celular", precio: 700 },
+];
+//comprobar si un valor existe en una rray
+// meses.forEach((mes) => {
+//   if (mes === "Enero") {
+//     console.log("Enero si existe");
+//   }
+// });
+
+//includes retorna un booleano si existe o no el mes en arreglo
+//NO SIRVE EN arreglo de objetos
+// const resultado = meses.includes("diciembre");
+
+// console.log(resultado);
+
+//En un arreglo de objetos se utiliza objetos
+const existe = carrito.some((producto) => {
+  return producto.nombre === "Si";
+});
+
+console.log(existe);
