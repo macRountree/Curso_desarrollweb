245d3c21aa44e71698c26947cd93c9067dd5584b
macrountree <mcrntree@gmail.com>
2023-05-29 09:52:05 -0700

    enviaremail

--- Desarrollo Web Completo/JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Desarrollo Web Completo/JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,6 +1,9 @@
 //?====================ENVIAR EMAIL=====================
 
 document.addEventListener("DOMContentLoaded", function () {
+  //Creamos un objeto para habilitar el boton de enviar
+  //los valores deben empezar vacios
+
   const email = {
     email: "",
     asunto: "",
@@ -12,7 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
   const form = document.querySelector("#formulario");
-
+  const btnSubmit = document.querySelector(
+    '#formulario button[type= "submit"] '
+  );
+  const btnReset = document.querySelector('#formulario button[type= "reset"] ');
+  const spinner = document.querySelector("#spinner");
   console.log(inputEmail);
   console.log(inputAsunto);
   console.log(inputMensaje);
@@ -21,9 +28,48 @@ document.addEventListener("DOMContentLoaded", function () {
   //Blur se puede utilizar para validar inputs que no se llenaron
   //añadimos validar como parametro sin mandar a llamar la funcion.. y nos desasemos de los
   //callbacks
-  inputEmail.addEventListener("blur", validar);
-  inputMensaje.addEventListener("blur", validar);
-  inputAsunto.addEventListener("blur", validar);
+  //input es un poco mas en tiempo real es mas rapido que
+  inputEmail.addEventListener("input", validar);
+  inputMensaje.addEventListener("input", validar);
+  inputAsunto.addEventListener("input", validar);
+  form.addEventListener("submit", enviarEmail);
+  btnReset.addEventListener("click", function (e) {
+    //prevenimos su accion default.. es decir reiniciar formulario de btnReset
+    e.preventDefault();
+    //Reiniciamos el objeto email
+    resetForm();
+  });
+
+  function enviarEmail(e) {
+    e.preventDefault();
+    spinner.classList.add("flex");
+    spinner.classList.remove("hidden");
+    setTimeout(() => {
+      spinner.classList.remove("flex");
+      spinner.classList.add("hidden");
+      resetForm();
+
+      //creamos alerta
+      const alertaExito = document.createElement("P");
+      alertaExito.classList.add(
+        "bg-green-500",
+        "text-white",
+        "p-2",
+        "text-center",
+        "rounded-lg",
+        "mt-10",
+        "font-bold",
+        "text-sm",
+        "uppercase"
+      );
+      alertaExito.textContent = "Mensaje Enviado";
+      form.appendChild(alertaExito);
+
+      setTimeout(() => {
+        alertaExito.remove();
+      }, 3000);
+    }, 3000);
+  }
 
   function validar(e) {
     //ParentElement muestra en consola el elemento padre de los inputs(en este caso)
@@ -39,10 +85,16 @@ document.addEventListener("DOMContentLoaded", function () {
         `El campo ${e.target.id} es obligatorio`,
         e.target.parentElement
       );
+
+      email[e.target.name] = "";
+      comprobarEmail();
       return;
     }
     if (e.target.id === "email" && !validarEmail(e.target.value)) {
       mostrarAlerta("El email no es valido", e.target.parentElement);
+
+      email[e.target.name] = "";
+      comprobarEmail();
       return;
     }
     limpiarAlerta(e.target.parentElement);
@@ -96,6 +148,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function comprobarEmail() {
     //includes verifica si uno de los valores del arreglo estan vacios
-    console.log(Object.values(email).includes(""));
+    //si hay valores que no estan vacios se quita habilita el boton enviar
+    //removiendo opacity y desabilitando disable de ese botonen html
+    if (Object.values(email).includes("")) {
+      btnSubmit.classList.add("opacity-50");
+      btnSubmit.disabled = true;
+      return;
+    }
+    btnSubmit.classList.remove("opacity-50");
+    btnSubmit.disabled = false;
+  }
+
+  function resetForm() {
+    email.email = "";
+    email.asunto = "";
+    email.mensaje = "";
+    form.reset();
+    comprobarEmail();
   }
 });
