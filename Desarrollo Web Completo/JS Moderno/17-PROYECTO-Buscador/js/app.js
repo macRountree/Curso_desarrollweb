76cc16f2d12a11d50b572e610f2005a3deefe6bc
Macrountree <a219206225@unison.mx>
2023-05-31 13:34:59 -0700

    buscadoryear

--- Desarrollo Web Completo/JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Desarrollo Web Completo/JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -3,6 +3,9 @@
 //========VARIABLES==============
 const resultado = document.querySelector("#resultado");
 const year = document.querySelector("#year");
+const max = new Date().getFullYear();
+const min = max - 10;
+
 //========LISTENERS===============
 document.addEventListener("DOMContentLoaded", () => {
   mostrarAutos();
@@ -34,4 +37,12 @@ function mostrarAutos() {
 
 //generamos los años con un select
 
-function llenarSelect() {}
+function llenarSelect() {
+  //recorre de LOS AUTOS DEL 2023 HASTA 2013
+  for (let i = max; i >= min; i--) {
+    const opt = document.createElement("option");
+    opt.value = i;
+    opt.textContent = i;
+    year.appendChild(opt); //agrega los años al select
+  }
+}
