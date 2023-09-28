09fb2af2e9098f02d495356129d53690aa08cb2c
Mac Rountree <a219206225@unison.mx>
2023-09-28 07:44:23 -0700

    Promises

--- Desarrollo Web Completo/Desarrollo_WebCompleto/IntroduccionJS/js/28.js
+++ Desarrollo Web Completo/Desarrollo_WebCompleto/IntroduccionJS/js/28.js
@@ -25,16 +25,27 @@ const producto3 = new Producto('LapTop',300, false);
 const producto4 = new Producto('Iphone',100, true);
 
 
-//?Herencia : Hereda las funciones o argumentos de una clase Padre a una clase hijo
-
-class Libro extends Producto{
-
-}
-
-const libro = Libro('JS la Rev', 120)
-
-
-
-
 console.log(producto3);
 console.log(producto4.formatearPrecio());
+
+//?Herencia : Hereda las funciones o argumentos de una clase Padre a una clase hijo
+//? En este ejemplo tenenmos a la clase hijo Libro su Padre seria Producto.. y el extends 
+//?significa que heredara todas las propiedas y funciones de Producto aunque libro pueda tener sus clases propias
+class Libro extends Producto{
+    constructor(nombre,precio,isbn){ /*?los parametros heredados son nombre y precio y los propios son el codigo isbn */
+        super(nombre,precio); //!si queremos heredar
+        this.isbn = isbn;
+    }
+    formatearProducto(){
+        /*Podemos super para traernos el metodo de la clase padre su return sera el mismo y concatenamos el isbn de la misma funcion hija */
+        return `${super.formatearProducto()} y su ISBN es ${this.isbn}` 
+
+    }
+
+}
+//! Instanciamos un nuevo libro con argumentos
+const libro = new Libro('JS la Rev', 120, '12312321')
+
+
+console.log(libro.formatearProducto()); /* imprimimos el libro con las funcion heredada y en consola el mensaje retornado
+33 */
---
+++ Desarrollo Web Completo/Desarrollo_WebCompleto/IntroduccionJS/js/30.js
@@ -0,0 +1,30 @@
+//==PROMISES
+
+//*TODAS LAS APIS MODERNAS UTILIZAn PROMISES
+//* Un promise refleja un valor que podra estar disponible ahora en un futuro o nunca
+
+
+
+//Definimos un promises
+//!Cuando se ejecuta un promise se pasan 2 valores automaticamente .. llamado resolve y reject
+const usuarioAutenticado = new Promise((resolve,reject)=>{
+    const auth = false;
+    if (auth) {
+        resolve('Usuario Auth'); //Sejecuta resolve cuando el promise se cumple
+    }else{
+        reject('Contraseña Incorrecta') //Se ejecuta si si el promies NO se cumple
+
+    }
+
+
+
+})
+
+
+console.log(usuarioAutenticado);
+/*En los promises existen 3 valores posibles
+Pending: No se ha cumplido pero no se rechaza
+Fulfilled: Se cumplio el promise
+Rejecto: se rechaza  o no se pudo ccumplir el Promise
+
+*/
