640e38838c23540db6930af1b59b8cd6c6e6ce5f
Mac Rountree <a219206225@unison.mx>
2023-10-13 08:03:22 -0700

    Alrta JS moderno 134

--- Desarrollo Web Completo/JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Desarrollo Web Completo/JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
   //Listeners
   //el blur se utiliza cuando tenemos selecciono el input (email) y cuando nos salimos se dispara el evento
@@ -18,9 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
   //En lugar de hacer 3 listeners hacemos 1 funcion para reutilizar codigo ..mandamos la funcion validar a cada listtener
 
   function validar(e) {
+    console.log(e.target.parentElement);
     if (e.target.value.trim() === "") {
       //trim elimina espacios en blanco y se recomienda utilizarlo en formularios
-      mostrarAlerta();
+      mostrarAlerta(
+        `El campo ${e.target.id} obligario`,
+        e.target.parentElement
+      );
     } else {
       console.log("Algo");
     }
@@ -30,11 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Funcion mostrar alerta
 
-  function mostrarAlerta() {
+  function mostrarAlerta(mensaje, referencia) {
     //GENERAMOS ALERTA EN html se recomienda que la etiqueta sea en mayusculas
     const error = document.createElement("P");
-    //! AQUI  VIDEO 130 JS moderno
-    error.textContent = "Campo Obligatorio";
+    error.textContent = mensaje;
+    //al error le agregamos una clase de fondo rojo para que se vea llamativo
+    error.classList.add("bg-red-600", "text-center", "text-white", "p-2");
+    //appendchidl agrega un nuevo elemento a lo ya existente
+    referencia.appendChild(error);
+
+    //Como el aviso de erro no es llamativo le agregamos estilos
+
     // console.log("ALEERTA");
   }
 });
