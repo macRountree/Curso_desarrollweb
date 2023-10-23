d905b5481e9e8234180e36072b52f40debeb7e99
Macrountree <a219206225@unison.mx>
2023-10-23 15:45:47 -0700

    tweets

--- Desarrollo Web Completo/JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Desarrollo Web Completo/JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -1,8 +1,8 @@
 //=======LOCAL STORAGE=================
 
 //===========VARIABLES =====================
-const form = document.querySelector("#formulario");
-const listaTweets = document.querySelector("#lista-tweets");
+const form = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
 
 //creamos una variable para los arreglos con un arreglo vacio
 // se almacenan los datos en una arreglo
@@ -12,14 +12,14 @@ eventListeners();
 
 function eventListeners() {
   //cuando el usuario agrega nuevo tweet
-  form.addEventListener("submit", agregarTweet);
+  form.addEventListener('submit', agregarTweet);
 
   //Cuando el doc esta listo
 
-  document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener('DOMContentLoaded', () => {
     //inteneta buscar en local storage tweets yconviertelos en JSON.parse
     // si marca null.. conviertelo a un array vacio
-    tweets = JSON.parse(localStorage.getItem("tweets") || []);
+    tweets = JSON.parse(localStorage.getItem('tweets') || []);
 
     crearHtml();
   });
@@ -30,12 +30,12 @@ function agregarTweet(e) {
   e.preventDefault();
 
   //text area
-  const tweet = document.querySelector("#tweet").value;
+  const tweet = document.querySelector('#tweet').value;
 
   //validar
 
-  if (tweet === "") {
-    MostrarError("No puede ir vacio");
+  if (tweet === '') {
+    MostrarError('No puede ir vacio');
     return; //con este return previene que se ejecuten otras lineas de codigo
     //el return solo sirve si esta dentro de una funcion
   }
@@ -61,14 +61,14 @@ function agregarTweet(e) {
 //mostrar error es una funcion que crea un parrafo con su clase
 // que se inyectara al html con un mensaje de error
 function MostrarError(error) {
-  const mensajeError = document.createElement("p");
+  const mensajeError = document.createElement('p');
 
   mensajeError.textContent = error;
-  mensajeError.classList.add("error");
+  mensajeError.classList.add('error');
 
   //insertamos en el contenido
 
-  const contenido = document.querySelector("#contenido");
+  const contenido = document.querySelector('#contenido');
 
   //el div contenido se agrega el mensaje hasta el final del dicho div
   contenido.appendChild(mensajeError);
@@ -84,24 +84,28 @@ function crearHtml() {
   if (tweets.length > 0) {
     limpiarHTML();
     //iteramos el arreglo
-    tweets.forEach((tweet) => {
+    tweets.forEach(tweet => {
       //agregamos un boton
-      const btnEliminar = document.createElement("a");
-      btnEliminar.classList.add("borrar-tweet");
-      btnEliminar.innerText = "X";
+      const btnEliminar = document.createElement('a');
+      const btnEditar = document.createElement('a');
+      btnEditar.classList.add('editar-tweet');
+      btnEditar.innerText = 'Editar';
+      btnEliminar.classList.add('borrar-tweet');
+      btnEliminar.innerText = 'X';
 
       //añadimos la funcion de eliminar
 
       btnEliminar.onclick = () => {
         borrarTweet(tweet.id);
       };
-      const li = document.createElement("li");
+      const li = document.createElement('li');
       //añadimos texto
 
       li.innerText = tweet.tweet;
 
       //asignamos el boton
       li.appendChild(btnEliminar);
+      li.appendChild(btnEditar);
 
       //mientras tenga el appendchild el codigo se repetira
       listaTweets.appendChild(li);
@@ -116,15 +120,20 @@ function crearHtml() {
 ///Agrega tweets al local storage
 
 function syncStorage() {
-  localStorage.setItem("tweets", JSON.stringify(tweets));
+  localStorage.setItem('tweets', JSON.stringify(tweets));
 }
 //le pasamos el id del objeto tweetObj
 function borrarTweet(id) {
   //para borrar un tweet creando un array
 
-  tweets = tweets.filter((tweet) => tweet.id != id);
+  tweets = tweets.filter(tweet => tweet.id != id);
 
   crearHtml();
+}
+
+//editarTweet
+function editarTweet(id) {
+  // ! crear modal que edite un tweet
 }
 
 //limpiar html
