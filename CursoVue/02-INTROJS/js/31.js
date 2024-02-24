14b31e96febe2fda0746f48fbcd6d7e99e1d569f
Mac Rountree <a219206225@unison.mx>
2024-02-24 08:26:57 -0700

    Actualización de scripts JS y consultas a API

--- CursoVue/02-INTROJS/js/31.js
+++ CursoVue/02-INTROJS/js/31.js
@@ -1,1 +1,23 @@
-const url = "https://jsonplaceholder.typicode.com/comments"
+const url = 'https://jsonplaceholder.typicode.com/comments';
+const url2 = 'https://jsonplaceholder.typicode.com/photos';
+
+//**  Consultar vairias API en un mismo proyecto */
+
+const consultarAPI = async () => {
+  const resp = await fetch(url);
+  const res = await resp.json();
+  console.log(res);
+  //* Como el await del segundo url debe esperar el primer url se recomienda crear otra funcion asincrona para el url2 O crear un promise.ALL para que se ejecuten las 2 consultas  almismo tiempo
+};
+
+const consultarAPI2 = async () => {
+  const [res, res2] = await Promise.all([fetch(url), fetch(url2)]);
+  const resultado = await res.json();
+  const resultado2 = await res2.json();
+
+  console.log(resultado);
+  console.log(resultado2);
+};
+
+consultarAPI2();
+consultarAPI();
--- CursoVue/02-INTROJS/js/32.js
+++ CursoVue/02-INTROJS/js/32.js
@@ -1,2 +1,28 @@
-const url = "https://jsonplaceholder.typicode.com/comments"
-const url2 = "https://jsonplaceholder.typicode.com/photos"
+const url = 'https://jsonplaceholder.typicode.com/comments';
+const url2 = 'https://jsonplaceholder.typicode.com/photos';
+
+// ASync Await
+//*Syntaxis async Aswait es mas corta que Promises
+
+const consultarAPI = async () => {
+  fetch(url)
+    .then(resp => resp.json())
+    .then(res => console.log(res));
+  // .catch(error => console.log(error));
+  // .finally(() => {
+  //   console.log('Listo');
+  // });
+
+  try {
+    //*podemos crear una variable
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    console.log('Liste');
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('Listo');
+  }
+};
+
+consultarAPI();
