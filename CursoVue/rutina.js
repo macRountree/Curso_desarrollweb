292d643ac396aef10175b7e513aad1f70d90d032
MacRountree <a219206225@unison.mx>
2024-02-25 19:34:20 -0700

    Agregadas imágenes y archivos de configuración para el proyecto de Vue.

---
+++ CursoVue/rutina.js
@@ -0,0 +1,40 @@
+/*
+ *=========RUTINA=========
+ */
+
+/*
+ 
+ * ===========LUNES=======
+ ?=====PECHO======
+    !Barra Plana 
+    !Barra Inclinada
+    !FondosPecho Biserie PoleaPechobajo
+    !Flies
+ ?=====Hombro/Antebrazo==
+    ! Empuje hombros sentado / Jalon muñecas Polea (Prono)
+    ! Polea Lateral / Antebrazo (Supino)  
+    ! 
+    
+ 
+
+ */
+/*
+ 
+ * ===========Martes=======
+ */
+/*
+ 
+ * ===========Miercoles=======
+ */
+/*
+ 
+ * ===========Jueves=======
+ */
+/*
+ 
+ * ===========Viernes=======
+ */
+/*
+ 
+ * ===========Sabado=======
+ */
